3ebbce31055bb5f9edf1bfa8123824c2754a577b
AnantSingh <anants98@yahoo.com>
2022-09-30 17:06:16 -0500

    added todo list of endpoints to index.js

--- index.js
+++ index.js
@@ -14,15 +14,27 @@ app.use(
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
+//TODO
+//app.get('/createGb',db.createGb)//creates a gameboard
+//app.get('/updateGb',db.updateGb)//updates a gameboard by ID
+//app.get('/deleteGb',db.deleteGb)//delete a gameboard
+//
+//app.get('/saveGb',db.saveGb)//saves a gameboard to users profile... thinking now i need to add a gameboard section in the users table.
+//app.get('/getUserGb')',db.getUserGb)//gets gameboards saved by a user by userID
+//
+//app.get('/getLb',db.getLb)//get the global leaderboard
+//app.get('/updateLb',db.updateLb)//update the leaderboard... this will be interesting. everytime a game is played, will have to check users W/L records and update
+//app.get('/getTopten',db.getTopten)//get the top ten of the leaderboard
 
 
-app.get('/getCats',db.getCats)
 
-app.get('/getUsers',db.getUsers)
-app.get('/getUserById',db.getUserById)
-app.get('/createUser',db.createUser)
-app.get('/updateUser',db.updateUser)
-app.get('/deleteUser',db.deleteUser)
+app.get('/getCats',db.getCats)//get categories + cardinality
+
+app.get('/getUsers',db.getUsers)//gets all users
+app.get('/getUserById',db.getUserById)//gets user by id
+app.get('/createUser',db.createUser)// creates a user
+app.get('/updateUser',db.updateUser)// updates a user
+app.get('/deleteUser',db.deleteUser)// deletes a user
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
